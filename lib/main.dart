799f226a435a7976f14d480c81f861118bece781
Deatsilence <mert_im2000@hotmail.com>
2023-12-16 16:10:25 +0300

    INFDIC-4 Project initialize logics added

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,13 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:infdic/feature/view/login_view.dart';
+import 'package:infdic/product/init/application_initialize.dart';
 import 'package:infdic/product/init/product_localization.dart';
 import 'package:infdic/product/init/theme/custom_dark_theme.dart';
 import 'package:infdic/product/init/theme/custom_light_theme.dart';
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await EasyLocalization.ensureInitialized();
+  await ApplicationInitialize().make();
 
   runApp(ProductLocalization(child: const MyApp()));
 }
