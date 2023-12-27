9fa9cf508e0aeee5bc4302674661fc12db6c0ccf
Deatsilence <mert_im2000@hotmail.com>
2023-12-28 01:22:15 +0300

    phone number can be added now on verification view (26)

--- lib/feature/view/auth/phone_number_verification_view.dart
+++ lib/feature/view/auth/phone_number_verification_view.dart
@@ -1,6 +1,5 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:common/common.dart';
-import 'package:country_picker/src/country.dart';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -9,6 +8,7 @@ import 'package:infdic/feature/mixin/phone_number_verification_view_mixin.dart';
 import 'package:infdic/feature/view/auth/base_auth_view.dart';
 import 'package:infdic/feature/view/widget/index.dart';
 import 'package:infdic/product/init/language/locale_keys.g.dart';
+import 'package:infdic/product/utility/extension/list_gutter_extension.dart';
 import 'package:infdic/product/utility/extension/padding_extension.dart';
 import 'package:infdic/product/widget/custom_text_form_field.dart';
 import 'package:sizer/sizer.dart';
@@ -45,11 +45,21 @@ class _PhoneNumberVerificationViewState
               LocaleKeys.auth_verification_body,
             ).tr(),
             CustomTextFormField(
+              hintText: LocaleKeys
+                  .general_text_form_field_please_enter_a_phone_number
+                  .tr(),
               keyboardType: TextInputType.phone,
-              inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                LengthLimitingTextInputFormatter(20),
+              ],
               prefixIcon: const PrefixPhoneNumberPicker(),
             ),
-          ],
+            AuthCustomElevatedButton(
+              onPressed: () {},
+              child: const Text('Send'),
+            ),
+          ].seperate(space: 1.h),
         ),
       ),
     );
